2c6230ecae50c626510bc168fe06c445c902517a
AlexeyKochkin <alexey.kochkin@devexpress.com>
2018-09-10 09:47:28 +0300

    add client-side sample for angular 5+

--- JS/ClientSide_Angular5+/src/app/app.component.ts
+++ JS/ClientSide_Angular5+/src/app/app.component.ts
@@ -5,8 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'app';
+  title = 'DXReportDesignerSample';
   reportUrl = "Products";
 }
--- JS/ClientSide_Angular5+/src/app/app.module.ts
+++ JS/ClientSide_Angular5+/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ReportDesignerComponent } from './report-designer/report-designer.component';
-
 
 @NgModule({
   declarations: [
