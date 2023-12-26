7f3fd7b4d2dfde47027f03cb53595dbbc3318a0c
Polina Tyureva <polina.tyureva@devexpress.com>
2023-12-26 17:47:15 +0400

    update client to angular v17

--- angular-report-designer/README.md
+++ angular-report-designer/README.md
@@ -1,6 +1,6 @@
 # AngularReportDesigner
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.8.
 
 ## Development server
 
--- angular-report-designer/src/app/app.component.ts
+++ angular-report-designer/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { DxReportDesignerModule } from 'devexpress-reporting-angular';
 import 'devexpress-reporting/dx-richedit';
 
 @Component({
-selector: 'app-root',
-encapsulation: ViewEncapsulation.None,
-templateUrl: './app.component.html',
-styleUrls: [
-    "../../node_modules/devextreme/dist/css/dx.light.css",
+  selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [
+	CommonModule, 
+	RouterOutlet,
+	DxReportDesignerModule],
+  templateUrl: './app.component.html',
+  styleUrls: [
+    "../../node_modules/ace-builds/css/ace.css",
+    "../../node_modules/ace-builds/css/theme/dreamweaver.css",
+    "../../node_modules/ace-builds/css/theme/ambiance.css",
+    "../../node_modules/devextreme-dist/css/dx.light.css",
     "../../node_modules/devexpress-richedit/dist/dx.richedit.css",
     "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
     "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
@@ -17,13 +28,13 @@ styleUrls: [
 })
 
 export class AppComponent {
-title = 'DXReportDesignerSample';
-// If you use the ASP.NET Core backend:
-getDesignerModelAction = "/DXXRD/GetDesignerModel"
-// If you use the ASP.NET MVC backend:
-//getDesignerModelAction = "/ReportDesigner/GetReportDesignerModel";
-// The report name.
-reportName = "TestReport";
-// The backend application URL.
-host = 'https://localhost:5001/';
+	title = 'DXReportDesignerSample';
+	// If you use the ASP.NET Core backend:
+	getDesignerModelAction = "/DXXRD/GetDesignerModel"
+	// If you use the ASP.NET MVC backend:
+	//getDesignerModelAction = "/ReportDesigner/GetReportDesignerModel";
+	// The report name.
+	reportName = "TestReport";
+	// The backend application URL.
+	host = 'https://localhost:5001/';
 }
